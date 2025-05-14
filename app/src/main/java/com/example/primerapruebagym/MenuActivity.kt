bc82174e03c9da1ea28a31013b14cff680f03b11
willy9461 <guillermogalarraga1986@gmail.com git config --global user.email guillermogalarraga1986@gmail.com>
2025-05-14 18:32:22 -0300

    creamos la pantalla de registro exitoso

--- app/src/main/java/com/example/primerapruebagym/MenuActivity.kt
+++ app/src/main/java/com/example/primerapruebagym/MenuActivity.kt
@@ -1,6 +1,8 @@
 package com.example.primerapruebagym
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class MenuActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val buttonRegistrarSocio = findViewById<Button>(R.id.registrarSocio)
+        buttonRegistrarSocio.setOnClickListener {
+            val intent = Intent(this, RegistarSocioActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/primerapruebagym/registroExitosoActivity.kt
+++ app/src/main/java/com/example/primerapruebagym/registroExitosoActivity.kt
@@ -1,25 +1,20 @@
 package com.example.primerapruebagym
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class RegistarSocioActivity : AppCompatActivity() {
+class registroExitosoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registar_socio)
+        setContentView(R.layout.activity_registro_exitoso)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val buttonVolverAtras = findViewById<Button>(R.id.buttonVolverAtras)
-        buttonVolverAtras.setOnClickListener {
-            finish()
         }
     }
 }
