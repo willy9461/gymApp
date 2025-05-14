8fce7db0f6c07820e5bc8e06f2d7382869addbdc
willy9461 <guillermogalarraga1986@gmail.com git config --global user.email guillermogalarraga1986@gmail.com>
2025-05-13 21:10:53 -0300

    creamos la tercera pantalla y agregamos funcionalidad desde el botton ingresar a la pantalla menu

--- app/src/main/java/com/example/primerapruebagym/RegistarSocioActivity.kt
+++ app/src/main/java/com/example/primerapruebagym/RegistarSocioActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Menu : AppCompatActivity() {
+class RegistarSocioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_registar_socio)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
